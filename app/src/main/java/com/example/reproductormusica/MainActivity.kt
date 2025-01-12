6d066c38825c5cd78ac9bd8e6159cacc10203986
Samuel Moreno <samuelalejandromorenopestana@gmail.com>
2025-01-12 21:04:54 +0100

    cambios

--- app/src/main/java/com/example/reproductormusica/MainActivity.kt
+++ app/src/main/java/com/example/reproductormusica/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.reproductormusica.ui.theme.ReproductorMusicaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ReproductorMusicaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReproductorMusicaTheme {
-        Greeting("Android")
+        setContentView(R.layout.main_activity)
     }
 }
